6e62be0090af5b601861192414757c50c2af4281
Louis Tricot <louis.tricot06@gmail.com>
2019-03-20 18:26:08 +0100

    portfolio

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Scroll section 1
+var btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', function() {
+// 	window.scroll({
+// 		top: window.innerHeight,
+// 		left: 0,
+// 		behavior: 'smooth',
+// 	});
+// });
+
+// Page CV - section formations
+// var dateForm1 = document.querySelector('.date-19');
+// var dateForm2 = document.querySelector('.date-18');
+// var dateForm3 = document.querySelector('.date-17');
+// var textForm = document.querySelector('.titre_form .texte_rouge');
+//
+// dateForm1.addEventListener('click', function() {
+// 	textForm.innerHTML = 'Troisième année Chef de projet Digital <br /> à l\'ECV Digital de Nantes ';
+// 	dateForm1.clasList.add('active');
+// });
+//
+// dateForm2.addEventListener('click', function() {
+// 	textForm.innerHTML = 'Deuxième année Chef de projet Digital <br /> à l\'ECV Digital de Nantes';
+// });
+//
+// dateForm3.addEventListener('click', function() {
+// 	textForm.innerHTML = 'Première année Chef de projet Digital <br /> à l\'ECV Digital de Nantes';
+// });
